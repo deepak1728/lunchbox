5ae406006d2854841b7262a296350c22fde4319b
deepak1728 <deepakmatta28@gmail.com>
2025-08-04 21:11:40 -0500

    bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Replace "lunchbox" with your actual GitHub repo name
 export default defineConfig({
+  base: "/lunchbox/",
   plugins: [react()],
   server: {
-    host: true, // allow external access
-    allowedHosts: [
-      "c846d630df88.ngrok-free.app", // your ngrok subdomain
-    ],
+    host: true, // Allows access from network (useful in local testing)
   },
 });
